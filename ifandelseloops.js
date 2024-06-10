e3c44506a1ee78412d2910539ab60e84fb1f347b
maadesh <151980884+Maadesh11@users.noreply.github.com>
2024-06-10 11:14:36 +0530

    Create ifandelseloops.js

---
+++ ifandelseloops.js
@@ -0,0 +1,13 @@
+let num=30;
+if(num%3==0){
+    document.write("divisible by 3"," ");
+}
+document.write("if else loop");
+var num1=20;
+if(num1%2==0){
+    document.write("divisible by 2");
+
+}
+else{
+    document.write("not divisible by 2");
+}
